6e45c1333e6d079c7d609a61fa04adae11b47c08
jys0101 <jys0101@Ubuntu.20.0.4>
2021-03-31 20:04:23 +0900

    simple vector test

---
+++ vector_test/simple_vector/vector/vector.cu
@@ -0,0 +1,62 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_DATA 512
+
+__global__ void vecAdd(int *_a, int *_b, int *_c)
+{
+	int tID = threadIdx.x;
+	_c[tID] = _a[tID] + _b[tID];
+}
+
+int main(void){
+	int *a, *b, *c;
+	int *d_a, *d_b, *d_c;
+
+	int memSize = sizeof(int)*NUM_DATA;
+	printf("%d elements, memSize = %d bytes\n", NUM_DATA, memSize);
+
+	a = new int[NUM_DATA]; memset(a, 0, memSize);
+	b = new int[NUM_DATA]; memset(b, 0, memSize);
+	c = new int[NUM_DATA]; memset(c, 0, memSize);
+	
+	for (int i =0; i< NUM_DATA; i++){
+		a[i] = rand() % 10;
+		b[i] = rand() % 10;
+	}
+	cudaMalloc(&d_a, memSize);
+	cudaMalloc(&d_b, memSize);
+	cudaMalloc(&d_c, memSize);
+	
+	//Under two line synchronize automatically. You don't need to use synchronize.
+	cudaMemcpy(d_a, a, memSize, cudaMemcpyHostToDevice); 
+	cudaMemcpy(d_b, b, memSize, cudaMemcpyHostToDevice);
+	
+	// Kernel call
+	vecAdd<<<1, NUM_DATA >>>(d_a, d_b, d_c);
+	cudaDeviceSynchronize();
+	cudaMemcpy(c, d_c, memSize, cudaMemcpyDeviceToHost);
+
+	//check results
+	bool result = true;
+	for (int i =0; i<NUM_DATA; i++)
+	{
+		if((a[i] + b[i]) != c[i]){
+			printf("[%d] The results is not matched! (%d, %d)\n",
+					i, a[i] + b[i], c[i]);
+			result = false;
+		}
+	}
+
+	if(result)
+		printf("GPU works well!\n");
+
+	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+	delete [] a; delete [] b; delete [] c;
+
+	return 0;
+}
